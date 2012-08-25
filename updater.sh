4cd6b70f8c64bc0f05e00fe7c9ba260f7cf9eb75
Daniel Bateman <jetison.24@gmail.com>
2012-08-25 00:25:02 -0500

    aries-common: support older/non-cwm recoveries when installing
    Lack of UPDATE_PACKAGE being set when updater.sh is run from stock
    was causing a loss of IMEI when installing.
    
    Thanks to festis74 from XDA for sending me a debug analysis of the
    IMEI not being restored.
    
    Change-Id: Ic00a95025f1069692878bbf4da1a0e3bbb538e58

--- updater.sh
+++ updater.sh
@@ -37,6 +37,12 @@ else
     DATA_PART='/dev/block/mmcblk0p2'
 fi
 
+# check for old/non-cwm recovery.
+if ! /tmp/busybox test -n "$UPDATE_PACKAGE" ; then
+    # scrape package location from /tmp/recovery.log
+    UPDATE_PACKAGE=`/tmp/busybox cat /tmp/recovery.log | /tmp/busybox grep 'Update location:' | /tmp/busybox tail -n 1 | /tmp/busybox cut -d ' ' -f 3-`
+fi
+
 # check if we're running on a bml, mtd (old) or mtd (current) device
 if /tmp/busybox test -e /dev/block/bml7 ; then
     # we're running on a bml device
